d82f1c5c2800a54177a5bb4bbf5c432c953e1ee7
YooniqueEque <97013492+YooniqueEque@users.noreply.github.com>
2022-04-12 18:38:42 +0900

    게시판 예시 & 패키지 생성

--- src/main/java/com/semi/heyearth/HeyearthApplication.java
+++ src/main/java/com/semi/heyearth/HeyearthApplication.java
@@ -2,8 +2,14 @@ package com.semi.heyearth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
+import board.BoardController;
 
 @SpringBootApplication
+@ComponentScan
+@ComponentScan(basePackageClasses = BoardController.class)
 public class HeyearthApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/webapp/WEB-INF/views/board/boardview.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="jquery-3.6.0.min.js"></script>
+<script>
+	$(document).ready(function () {
+		
+	});
+</script>
+<meta charset="UTF-8">
+<title>게시물 조회 | heyEarth</title>
+</head>
+<body>
+
+</body>
+</html>
