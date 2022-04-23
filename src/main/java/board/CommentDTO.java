adb7f8f4333693fe3868acebad6c5b6a1ab4d5d0
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-04-23 19:48:11 +0900

    주말 소영

--- src/main/java/board/CommentDTO.java
+++ src/main/java/board/CommentDTO.java
@@ -22,25 +22,25 @@ public class CommentDTO {
 	}
 
 
-	public int getB_no() {
+	public int getB_no() { //게시물번호 
 		return b_no;
 	}
-	public void setB_no(int b_no) {
+	public void setB_no(int b_no) { 
 		this.b_no = b_no;
 	}
-	public int getC_index() {
+	public int getC_index() {//댓글 인덱스 
 		return c_index;
 	}
 	public void setC_index(int c_index) {
 		this.c_index = c_index;
 	}
-	public int getC_order() {
+	public int getC_order() {//댓글과 대댓글 순서
 		return c_order;
 	}
 	public void setC_order(int c_order) {
 		this.c_order = c_order;
 	}
-	public String getC_comment() {
+	public String getC_comment() {//댓글 내용
 		return c_comment;
 	}
 	public void setC_comment(String c_comment) {
