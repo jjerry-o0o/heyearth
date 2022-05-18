b45da3965fe6befedb96fca9ab59535dfcdf4ca4
alfndp <alfndp25@gmail.com>
2022-05-19 08:59:46 +0900

    .

--- src/main/java/zerokit/ZerokitController.java
+++ src/main/java/zerokit/ZerokitController.java
@@ -4,13 +4,11 @@ package zerokit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import participation.ParticipationService;
-import participation.ParticipationServiceImpl;
+
 
 @Controller
 public class ZerokitController {
@@ -18,12 +16,6 @@ public class ZerokitController {
 	@Autowired
 	@Qualifier("zerokitservice")
 	ZerokitService zerokitservice = new ZerokitServiceImpl();
-
-	// 제로키트 페이지
-//	@GetMapping("/zerokit")
-//	public String zerokitlist() {
-//		return "zerokit/zerokit";
-//	}
 	
 @RequestMapping(value = "/zerokit", method = RequestMethod.GET)
 public ModelAndView zerokit(ModelAndView mav) {
