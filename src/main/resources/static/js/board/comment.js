54548f3a039593ec3fe595e6046a1e2a23140565
YooniqueEque <97013492+YooniqueEque@users.noreply.github.com>
2022-05-23 17:41:55 +0900

    last

--- src/main/resources/static/js/board/comment.js
+++ src/main/resources/static/js/board/comment.js
@@ -14,8 +14,7 @@
 						
 						if($(".id").val() == data[i].id){
 							$("#tr2"+i).append("<td class='td2'><button class='button' id='alter_btn"+i+"' onclick='alter("+i+")'>수정</button></td>"
-							 + "<td class='td2'><button class='button' id='delete_btn"+i+"' onclick='remove("+i+")'>삭제</button>"
-							 + "<button class='button' id='recom_btn"+i+"' onclick='insert2()'>댓글</button></td>");
+							 + "<td class='td2'><button class='button' id='delete_btn"+i+"' onclick='remove("+i+")'>삭제</button>");
 						}
 						
 						$("#list_div").append("<input type='hidden' id='c_index"+i+"' name='c_index' value='"+ data[i].c_index+"'><div class='alter_div' id='alter_div"+i+"'></div>");
@@ -40,18 +39,6 @@
 		});
 	}
 	
-	function insert2() {
-		$.ajax({
-			type: "post",
-			url: "/insertcomment",
-			data: { "id": $(".id").val(), "c_comment": $("#c_comment").val(), "b_no": $(".b_no").val(), "c_order":$(".c_order").val()},
-			success: function() {
-				$("#c_comment").val("");
-				$("#comment_tb").empty();
-				selectlist();
-			}
-		});
-	}
 	
 	function alter(i){
 		$("#alter_div"+i).append("<form class='calter_div'><h3 class='comment_h'>'"+$("#c_comment"+i).val()+"'을<br>수정중입니다.</h3>"
@@ -60,6 +47,7 @@
 		+ "<input type='button' class='button' id='alter"+i+"' value='수정등록' onclick='update("+i+")'>"
 		+ "</form>");
 	}
+	
 	
 	function update(i){
 		alert("댓글이 수정되었습니다.");
